403379b226431291a4861846785ac57f75d1e2ec
KaisarH007 <kaisarh007@gamil.com>
2021-10-31 11:34:29 +0600

    modify

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ const port = process.env.PORT || 7000;
 app.use(cors());
 app.use(express.json());
 
-//user name =dreamyTour
-//user pass =cSescxiFfoMrMsv2
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7gwaa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
